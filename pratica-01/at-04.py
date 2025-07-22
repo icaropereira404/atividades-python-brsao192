bd44938f70035045946f61453f5cc4731c7e6970
icaropereira404 <icarop995@gmail.com>
2025-07-21 21:09:29 -0300

    Atividades respondidas do 1 ao 4 da Prática 1

---
+++ pratica-01/at-04.py
@@ -0,0 +1,12 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("--- Detalhes da Compra ---")
+print("Nome do Produto:", nome_produto)
+print("Preço Unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("--------------------------")
+print("Preço Total: R$", preco_total)
